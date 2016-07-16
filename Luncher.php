c04eeb8358be82500bf96c1b589f14f9138d0573
poriya1 <lovinggg1@gmail.com>
2016-07-16 22:33:42 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '259753494:AAF-dmmUzh0bUgOLNEIMSOI8x1pEPjypol8');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
